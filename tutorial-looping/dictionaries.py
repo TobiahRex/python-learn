9065e8fe2381b59ee08a6ca70ae2ae4f88e6459b
TobiahRex <tobiah.rex@gmail.com>
2020-02-11 19:42:44 -0800

    CHECKPOINT / list comprehension
     -------------------------------------------
      MAIN
      * Adds list comprehension from object
      to list based on <dict>.items()
     -------------------------------------------

--- tutorial-looping/dictionaries.py
+++ tutorial-looping/dictionaries.py
@@ -9,9 +9,20 @@ def main():
     for key in prices.keys():
         prices[key] = round(prices[key] + .1, 2)
 
-    print(prices)
+    #------------------------------------------------
 
 
+    all_jedis = {
+        "obi-wan": "jedi knight",
+        "qui gon jin": "jedi knight",
+        "yoda": "jedi master",
+        "mace windu": "jedi master",
+    }
+
+    jedi_masters_list = [key for key, value in all_jedis.items() if "master" in all_jedis[key]]
+
+
+    print(jedi_masters_list)
 
 
 
